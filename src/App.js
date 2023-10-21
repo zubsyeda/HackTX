e4dcf2ab3dccddb02bd996ae362cab6adab3c76a
Ashley Kim <ashleykim@Ashleys-MacBook-Air-61.local>
2023-10-21 18:23:23 -0500

    dropdown2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Dropdown from './components/dropdown';
+//import Dropdown from './components/dropdown';
 
 // function MyButton() {
 //   return (
@@ -20,7 +20,16 @@ function App() {
   return (
     <div className="App">
       <h1 className="header">Welcome to ProjectName</h1>
-      <Dropdown />
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Select company
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">Ask_Spectrum</a>
+          <a class="dropdown-item" href="#">AmazonHelp</a>
+          <a class="dropdown-item" href="#">AppleSupport</a>
+        </div>
+      </div>
       <InputChatBox/>
       <button className="send"><img alt="Send" /></button>
     </div>
