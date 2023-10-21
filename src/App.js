5bf11357782ca339be8a2ad9af1cc7015678198a
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-10-21 16:51:16 -0500

    new app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>Welcome to CustomerSupports Support</h1>
+      <MyButton/>
     </div>
   );
 }
