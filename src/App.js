4066688fd8ab9aa9b522d2d4818f25c5fb3435e3
zubsyeda <125157533+zubsyeda@users.noreply.github.com>
2023-10-21 17:03:28 -0500

    new...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
+// function MyButton() {
+//   return (
+//     <button>I'm a button</button>
+//   );
+// }
 
 function InputChatBox() {
   return (
@@ -17,8 +17,9 @@ function InputChatBox() {
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to CustomerSupports Support</h1>
+      <h1 className="header">Welcome to CustomerSupports Support</h1>
       <InputChatBox/>
+      <button className="send"><img alt="Send" /></button>
     </div>
   );
 }
