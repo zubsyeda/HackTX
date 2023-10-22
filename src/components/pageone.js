24b2c1da439ff0635309d5a13501d686b2491f83
Ashley Kim <ashleykim@Ashleys-MacBook-Air-61.local>
2023-10-21 23:47:23 -0500

    sentiments

--- src/components/pageone.js
+++ src/components/pageone.js
@@ -1,6 +1,24 @@
 // PageOne.js
-import React from 'react';
+import React, { useState } from "react";
+import Sentiment from 'sentiment';
 //import TableauReport from 'tableau-react';
+
+function getSentiments() {
+  file = "twcs.csv"
+  var array = file.split("\n")
+  var scores = [];
+  var headers;
+  headers = lines[0].split(",");
+  for (var i = 1; i < array.length; i++) {
+    if (array[i][2] == FALSE) {
+      var tweet = array[i][4].substring(0,array[i][4].indexOf(" "));
+      var Sentiment = require('sentiment');
+      var sentiment = new Sentiment();
+      var score = sentiment.analyze(tweet);
+      scores.push(score);
+    }
+  }
+};
 
 function PageOne() {
   return (
